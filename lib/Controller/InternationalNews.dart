33ee01c3b5b843ca10a8c230c1674104323e35e5
Yazdani <yaz4dani@gmail.com>
2019-09-10 16:28:26 +0600

    Second Commit

---
+++ lib/Controller/InternationalNews.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InternationalNews extends StatefulWidget {
+  @override
+  _InternationalNewsState createState() => new _InternationalNewsState();
+}
+
+class _InternationalNewsState extends State<InternationalNews> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+
+      
+
+    );
+  }
+}
+
+
+
+
+
+
+
