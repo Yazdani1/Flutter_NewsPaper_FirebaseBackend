641b594b99197939c4c47891eef69f424e8a5336
Yazdani <yaz4dani@gmail.com>
2019-09-13 00:36:53 +0600

    Second Commit

--- lib/View/InternationalNews/In_news_gallery.dart
+++ lib/View/InternationalNews/In_news_gallery.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dart:async';
 
 class In_Gallery extends StatefulWidget {
   @override
@@ -6,10 +8,49 @@ class In_Gallery extends StatefulWidget {
 }
 
 class _In_GalleryState extends State<In_Gallery> {
+
+  StreamSubscription<QuerySnapshot>subscription;
+
+  List<DocumentSnapshot>snapshot;
+
+  CollectionReference collectionReference=Firestore.instance.collection("InternationalNews");
+
+  @override
+  void initState() {
+
+    subscription=collectionReference.snapshots().listen((datasnap){
+      setState(() {
+        snapshot=datasnap.documents;
+      });
+    });
+    super.initState();
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Color(0xFF222240),
+      
+      body: new ListView.builder(
+        itemCount: snapshot.length,
+        itemBuilder: (context,index){
+
+          return Container(
+            margin: EdgeInsets.all(10.0),
+            height: 300.0,
+            child: new Stack(
+              children: <Widget>[
+
+
+
+              ],
+            ),
+          );
+
+        }
+      ),
+
     );
   }
 }
