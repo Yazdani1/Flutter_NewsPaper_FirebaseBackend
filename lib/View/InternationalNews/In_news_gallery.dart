aea01d93fd01b30be1c0a8787d7d263a55c77e4c
Yazdani <yaz4dani@gmail.com>
2019-09-17 12:18:15 +0600

    Second Commit

--- lib/View/InternationalNews/In_news_gallery.dart
+++ lib/View/InternationalNews/In_news_gallery.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 
+
 class In_Gallery extends StatefulWidget {
   @override
   _In_GalleryState createState() => new _In_GalleryState();
